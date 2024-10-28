e38d1f501ecc670bc4deb03232dca714249cd37b
danyalb5 <danyalbawani@gmail.com>
2024-10-27 21:05:45 -0700

    Converted Lab 23 to use std::set for Lab 24

--- #include <iostream>.cpp
+++ #include <iostream>.cpp
@@ -16,94 +16,58 @@ void delete_data(vector<string>& vec, list<string>& lst, set<string>& st);
 
 
 int main() {
-    vector<string> vec;
-    list<string> 1st;
     set<string> st;
-
     string filename= "data.txt";
 
-    //Reading Race
-    auto read_time = time_operation([&])() { read_data(vec, 1st, st, filename); };
-    
-    //sorting race
-    auto sort_time_vector = time_operation([&]() { sort(vec.begin(), vec.end()); });
-    auto sort_time_list = time_operation([&]() { lst.sort(); });
-    int sort_time_set = -1; 
+    // Reading race
+    auto read_time = time_operation([&]() { read_data(st, filename); });
 
-    //inserrtion race
-    auto insert_time_vector = time_operation([&]() { insert_data(vec, lst, st); });
+    // Sorting race (sets are already sorted)
+    int sort_time_set = -1; // Sorting not needed, so we don't measure time
 
-    //Deletion race 
-    auto delete_time_vector = time_operation([&]() { delete_data(vec, lst, st); });
+    // Insertion race
+    auto insert_time_set = time_operation([&]() { insert_data(st); });
+
+    // Deletion race
+    auto delete_time_set = time_operation([&]() { delete_data(st); });
 
     // Print results
-    cout << " Operation    Vector(ms)  List(ms)  Set(ms)" << endl;
-    cout << "------------------------------------------" << endl;
-    cout << "    Read      " << read_time << "      " << read_time << "      " << read_time << endl;
-    cout << "   Sort       " << sort_time_vector << "       " << sort_time_list << "       " << sort_time_set << endl;
-    cout << "  Insert      " << insert_time_vector << "       " << insert_time_vector << "       " << insert_time_vector << endl;
-    cout << "  Delete      " << delete_time_vector << "       " << delete_time_vector << "       " << delete_time_vector << endl;
-
-
-    
+    cout << " Operation    Set(ms)" << endl;
+    cout << "------------------------" << endl;
+    cout << "    Read      " << read_time << endl;
+    cout << "   Sort       " << sort_time_set << endl;
+    cout << "  Insert      " << insert_time_set << endl;
+    cout << "  Delete      " << delete_time_set << endl;
 
     return 0;
 }
 
-//Function to measure execuition time 
+// Function to measure execution time
 template <typename Func>
 long long time_operation(Func func) {
     auto start = high_resolution_clock::now();
     func();
-    auto end = high_resolution_clocl::now();
-    return duration_cast <milliseconds>(end - start).count();
+    auto end = high_resolution_clock::now();
+    return duration_cast<milliseconds>(end - start).count();
 }
 
-//Function to read data into the data structures
-
-void read_data(vector<string>& vec, list<string>& 1st, set<string>& st, const string& filename) {
+// Function to read data into the set
+void read_data(set<string>& st, const string& filename) {
     ifstream file(filename);
     string code;
     while (file >> code) {
-        vec.push_back(code);
-        lst.push_back(code);
         st.insert(code);
-
     }
 }
-//Function to sort data in the vector and in the list 
-void sort_data(vector<string>& vec, list<string>& lst, set<string>& st) {
-    sort(vec.begin(), vec.end());
-    lst.sort();
-}
 
-// Function to insert "TESTCODE" in the middle of each data structure
-void insert_data(vector<string>& vec, list<string>& lst, set<string>& st) {
+// Function to insert "TESTCODE" into the set
+void insert_data(set<string>& st) {
     string testValue = "TESTCODE";
-    
-    // Insert in the middle of vector
-    vec.insert(vec.begin() + vec.size() / 2, testValue);
-
-    // Insert in the middle of list
-    auto listIt = lst.begin();
-    advance(listIt, lst.size() / 2);
-    lst.insert(listIt, testValue);
-
-    // Insert in set (does not care about position because it’s ordered)
-    st.insert(testValue);
+    st.insert(testValue); // Inserts in set, automatically positioned
 }
 
-// Function to delete the middle element from each data structure
-void delete_data(vector<string>& vec, list<string>& lst, set<string>& st) {
-    // Delete from middle of vector
-    vec.erase(vec.begin() + vec.size() / 2);
-
-    // Delete from middle of list
-    auto listIt = lst.begin();
-    advance(listIt, lst.size() / 2);
-    lst.erase(listIt);
-
-    // Delete from set (remove the TESTCODE)
+// Function to delete "TESTCODE" from the set
+void delete_data(set<string>& st) {
     st.erase("TESTCODE");
 }
 
